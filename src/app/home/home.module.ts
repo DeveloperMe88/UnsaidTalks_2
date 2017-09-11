6d533c6ef960189022af065c538c1ba1fc30ca7f
Sahil Gogna <sahil.gogna@nagarro.com>
2017-09-11 16:54:28 +0530

    improved navigations and body added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { HomeComponent } from './home.component'
+import { RouterModule } from '@angular/router'
+import {ContactComponent} from 'app/contact/contact.component'
+import {InterviewsComponent} from 'app/interviews/interviews.component'
+import {BlogComponent} from 'app/blog/blog.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild( [
+                  { path: 'contact', component:  ContactComponent},
+                  { path: 'blog', component:  BlogComponent},
+                  { path: 'interviews', component:  InterviewsComponent}
+              ] )
   ],
   declarations: [HomeComponent]
 })
